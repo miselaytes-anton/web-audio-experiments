084d453d3d6935bbd8674d3dfb3248b29907b98b
miselaytes-anton <a.miselaytes@gmail.com>
2018-08-21 15:38:32 +0200

    fix(guitar app): clean up css and src/index.js

--- packages/guitar-app/src/index.js
+++ packages/guitar-app/src/index.js
@@ -14,13 +14,14 @@ const virtualAudioGraph = createVirtualAudioGraph({
   output: audioContext.destination,
 });
 
-const declarativeUpdate = state => {
-  virtualAudioGraph.update(stateToGraph(state));
-};
+// update using virtualAudioGraph
+const updateAudioNodes = state => virtualAudioGraph.update(stateToGraph(state));
 
+// update using plain Web Audio API
 const nodes = {};
-const imperativeUpdate = state => {
+const updateAudioNodes2 = state => {
   if (isEmpty(nodes)) {
+    // creating nodes
     nodes.volume = audioContext.createGain();
     nodes.distortion = audioContext.createWaveShaper();
     nodes.distortion.oversample = state.distortion[2].oversample;
@@ -28,33 +29,31 @@ const imperativeUpdate = state => {
     nodes.guitar.buffer = state.guitar[2].buffer;
     nodes.guitar.loop = state.guitar[2].loop;
     nodes.guitar.start();
+
+    // connecting nodes
+    nodes.guitar.connect(nodes.distortion);
+    nodes.distortion.connect(nodes.volume);
+    nodes.volume.connect(audioContext.destination);
   }
 
-  // connecting the nodes
-  // but actually we also need to check if new ones were added or old ones removed
-  nodes.guitar.connect(nodes.distortion);
-  nodes.distortion.connect(nodes.volume);
-  nodes.volume.connect(audioContext.destination);
-
-  // setting the values
-  // but actually there is a lot of things we would need to check here
+  // updating params
   nodes.volume.gain.value = state.volume[2].gain;
   nodes.distortion.curve = state.distortion[2].curve;
 }
 
 getGuitarSoundBuffer(audioContext)
 .then(buffer => {
-
+  // we start with 0 distortion
+  const curve = makeDistortionCurve(0)
   const initialState = {
-    volume: ['gain', 'output', {gain: 0.5}],
-    distortion: ['waveShaper', 'volume', {curve: makeDistortionCurve(0), oversample: '4x'}],
+    // we start with 0 volume
+    volume: ['gain', 'output', {gain: 0}],
+    distortion: ['waveShaper', 'volume', {curve, oversample: '4x'}],
     guitar: ['bufferSource', 'distortion', {buffer, loop: true}],
   };
 
   const store = createStore(reducer, initialState);
-
-  //store.subscribe(() => declarativeUpdate(store.getState()))
-  store.subscribe(() => imperativeUpdate(store.getState()))
+  store.subscribe(() => updateAudioNodes(store.getState()))
 
   ui.init(store);
 });
