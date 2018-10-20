d1557629c6b12e46e3cf0d1c3bb6f40fbd8acf6f
miselaytes-anton <a.miselaytes@gmail.com>
2018-10-20 20:35:40 +0200

    feat(tape): initial setup

---
+++ packages/tape-app/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App" >
+        hello
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ packages/tape-app/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      title: 'Tape app',
+      template: 'index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // creates style nodes from JS strings
+          'style-loader',
+          // translates CSS into CommonJS
+          'css-loader',
+          // compiles Sass to CSS, using Node Sass by default
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+};
