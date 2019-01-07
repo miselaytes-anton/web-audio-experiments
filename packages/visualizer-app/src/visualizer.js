fd6af691b7a263f016ff7c5be174c3f8c178d2c2
miselaytes-anton <a.miselaytes@gmail.com>
2019-01-07 11:22:56 +0100

    feat: make lines zig-zag

--- packages/visualizer-app/src/visualizer.js
+++ packages/visualizer-app/src/visualizer.js
@@ -13,7 +13,8 @@ const featureRanges = {
 const shapeRanges = {
   circlePosition: [10, 40],
   scalingCoef: [1.8, 3],
-  R: [5, 20]
+  R: [5, 20],
+  zigZagK: [0, 100]
 };
 
 const mapParamRange = (feature, shape, v) => mapRange(featureRanges[feature], shapeRanges[shape], v);
@@ -23,37 +24,55 @@ const draw = canvasContext => {
   const h = window.innerHeight;
   canvasContext.canvas.width = w;
   canvasContext.canvas.height = h;
+  canvasContext.lineWidth = 0.5;
+  canvasContext.strokeStyle = `rgba(255,255,255, 1)`;
 
   return features => {
     if (features === null) {
       return;
     }
-    const {frequencyData, rms} = features;
     logTimes(features, 10);
 
-    canvasContext.lineWidth = 1;
-    canvasContext.strokeStyle = `rgba(255,255,255, 1)`;
+    const {frequencyData, rms} = features;
     canvasContext.beginPath();
     canvasContext.clearRect(0, 0, w, h);
 
     const bufferLength = frequencyData.length;
-    const scalingCoef = 2 || mapParamRange('rms', 'scalingCoef', rms);
+    const scalingCoef = 1.5;
     const angleStep = 2 * Math.PI / bufferLength;
-    const R = mapParamRange('rms', 'R', rms);
+    const zigZagK = mapParamRange('rms', 'zigZagK', rms);
+    const R = 100;
+    const numLineParts = 10;
     const circleCenter = {
       x: w / 2,
-      y: h / 2 - 80
+      y: h / 2 + 40
     };
 
     for (let i = 0; i < bufferLength; i++) {
       //reflect in the middle
       const index = i < bufferLength / 2 ? i : bufferLength - i;
-      //take every even bar
-      const v = frequencyData[index] * scalingCoef;
-      const x = circleCenter.x + Math.sin(angleStep * i) * (R + v);
-      const y = circleCenter.y + Math.cos(angleStep * i) * (R + v);
-      canvasContext.moveTo(circleCenter.x, circleCenter.y);
-      canvasContext.lineTo(x, y);
+
+      const v = Math.abs(frequencyData[index]) * scalingCoef;
+      const coords = [];
+      let direction = i % 2 === 0 ? 1 : -1;
+      const startCoord = [
+        circleCenter.x + Math.sin(angleStep * i) * R,
+        circleCenter.y + Math.cos(angleStep * i) * R
+      ];
+      for (let lineIndex = 0; lineIndex < numLineParts; lineIndex++) {
+        const start = lineIndex === 0
+          ? startCoord
+          : coords[lineIndex - 1];
+        direction *= -1;
+        coords.push([
+          start[0] + Math.sin(angleStep * (i + direction * zigZagK)) * v / numLineParts,
+          start[1] + Math.cos(angleStep * (i + direction * zigZagK)) * v / numLineParts
+        ]);
+      }
+      canvasContext.moveTo(...startCoord);
+      coords.forEach(([x, y]) => {
+        canvasContext.lineTo(x, y);
+      });
     }
 
     canvasContext.stroke();
