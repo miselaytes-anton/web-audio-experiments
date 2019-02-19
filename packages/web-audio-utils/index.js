c7dcc75168f921fec0d35633a6c59442c81ca143
miselaytes-anton <a.miselaytes@gmail.com>
2019-02-19 21:40:56 +0100

    feat(voice): add removeSilence util

--- packages/web-audio-utils/index.js
+++ packages/web-audio-utils/index.js
@@ -1,16 +1,15 @@
-const tracks = require('./tracks');
-const sample = require('lodash.sample');
-const mergeParams = require('./mergeParams');
-const CompositeAudioNode = require('./CompositeAudioNode');
-const getRandomTrack = () => `https://amiselaytes.com/lomax/audio/${sample(tracks)}.mp3`;
+import tracks from './tracks';
+import sample from 'lodash.sample';
 
-const getAudioBuffer = (audioCtx, url) => {
+export const getRandomTrack = () => `https://amiselaytes.com/lomax/audio/${sample(tracks)}.mp3`;
+
+export const getAudioBuffer = (audioCtx, url) => {
   return fetch(url)
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => audioCtx.decodeAudioData(arrayBuffer));
 };
 
-const getLiveAudio = (audioCtx, opts = {}) => navigator.mediaDevices.getUserMedia({audio: {
+export const getLiveAudio = (audioCtx, opts = {}) => navigator.mediaDevices.getUserMedia({audio: {
   autoGainControl: false,
   echoCancellation: false,
   noiseSuppression: false,
@@ -18,16 +17,53 @@ const getLiveAudio = (audioCtx, opts = {}) => navigator.mediaDevices.getUserMedi
 }})
 .then(stream => audioCtx.createMediaStreamSource(stream));
 
-const getAudioContext = () => {
+export const getAudioContext = () => {
   const Ctx = window.AudioContext || window.webkitAudioContext;
   return Ctx ? new Ctx() : null;
 };
 
-module.exports = {
-  mergeParams,
-  CompositeAudioNode,
-  getLiveAudio,
-  getAudioBuffer,
-  getRandomTrack,
-  getAudioContext,
+const closestPowerOf2 = number => 2 ** Math.floor(Math.log2(number));
+
+const msToNumSamples = (ms, sampleRate) => sampleRate * ms / 1000;
+
+export const signalToFrames = (signal, sampleRate, {frameLengthMs = 25, overlapLengthMs = 10}) => {
+  const frameLengthSamples = closestPowerOf2(msToNumSamples(frameLengthMs, sampleRate));
+  const overlapLengthSamples = overlapLengthMs ? closestPowerOf2(msToNumSamples(overlapLengthMs, sampleRate)) : 0;
+  let frames = [];
+  for (let i = 0; i <= signal.length - frameLengthSamples; i += (frameLengthSamples - overlapLengthSamples)) {
+    frames.push(signal.slice(i, i + frameLengthSamples));
+  }
+  return frames;
+};
+
+const sum = arr => arr.reduce((sum, curr) => sum + curr, 0);
+const avg = arr => sum(arr) / arr.length;
+const getRms = signal => avg(signal.map(v => v ** 2)) ** 1 / 2;
+const toDb = v => 20 * Math.log10(v);
+export const removeSilence = (signal, sampleRate, opts = {}) => {
+  const {threshold = 0, minSilenceDuration = 500} = opts;
+  // split into 25 ms frames
+  const frameLengthMs = 25;
+  const frames = signalToFrames(signal, sampleRate, {frameLengthMs, overlapLengthMs: 0});
+  const rmsPerFrame = frames.map(getRms).map(toDb);
+  // combine consequent silence chunks into silence groups
+  const silenceGroups = rmsPerFrame.reduce((groups, rms, index) => {
+    const activeGroup = groups[groups.length - 1];
+    if (rms <= threshold) {
+      // extending active silence group
+      activeGroup.push(index);
+    } else if (activeGroup.length) {
+      //previous group ended
+      groups.push([]);
+    }
+    return groups;
+  }, [[]]);
+
+  const longGroups = silenceGroups.filter(group => group.length * frameLengthMs > minSilenceDuration);
+  // filter away parts of the signal which are part of one of the silence groups
+  const numSamplesInFrame = Math.floor(signal.length / frames.length);
+  return signal.filter((v, i) => {
+    const frameIndex = Math.floor(i / numSamplesInFrame);
+    return !longGroups.some(group => group.includes(frameIndex));
+  });
 };
---
+++ packages/web-audio-utils/index.test.js
@@ -0,0 +1,31 @@
+import assert from 'assert';
+import {removeSilence, signalToFrames} from './index.js';
+
+describe('removeSilence', () => {
+  it('should removeSilence', () => {
+    const signal = [0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0];
+    const sampleRate = 40;
+    const expectedSignal = [1, 1, 1, 0, 1, 1, 1, 1];
+    const withoutSilence = removeSilence(signal, sampleRate, {minSilenceDuration: 25, threshold: 0.1})
+    assert.deepStrictEqual(withoutSilence, expectedSignal);
+  });
+});
+
+describe('signalToFrames', () => {
+  it('is should split signal to frames', () => {
+    const signal = [1, 2, 3, 4, 5, 6, 7, 8];
+    const sampleRate = 4;
+    const frameLengthMs = 500;
+    const overlapLengthMs = 250;
+    const expectedFrames = [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8]
+    ];
+    assert.deepStrictEqual(signalToFrames(signal, sampleRate, {frameLengthMs, overlapLengthMs}), expectedFrames);
+  });
+});
